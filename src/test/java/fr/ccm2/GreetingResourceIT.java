08fbb02df03598dabe4a68927326f4934fa09b33
quarkusio <no-reply@quarkus.io>
2024-01-19 10:18:16 +0000

    Initial commit

---
+++ src/test/java/fr/ccm2/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package fr.ccm2;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
